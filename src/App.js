20ecde6ea3e7819588c05cea190bfb8da0cfa111
JaiNarayanan <47904405+JaiNarayanan@users.noreply.github.com>
2022-10-05 19:39:04 -0400

    profile page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import IconBar from './components/IconBar';
+import Biography from './components/Biography';
 
 const Portfolio = () => {
   return(
     <>
       <div className="bg-primary-gray h-full">
         <Navbar />
-        <IconBar />  
+        <IconBar />
+        <div id="content" className="px-40">
+          <Biography />  
+        </div>
       </div>
     </>  
   )
---
+++ src/components/Biography.js
@@ -0,0 +1,24 @@
+import { Fragment } from 'react'
+import profile from '../assets/me2.png'
+
+const Navbar = () => {
+  return (
+    <>
+        <section className="px-80 pt-36">
+            <div class="grid-container grid grid-cols-6">
+                <div class="item2 col-span-4">
+                    <div className='mb-10 text-3xl text-white after:'>
+                        About Me
+                    </div>
+                </div>
+                <div class="item1 col-span-2 pt-20">
+                    <img className="rounded-md h-64 w-64" />                    
+                </div>
+
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Navbar;
