8c371305398ca1e06773a87ce81dc538fd981d94
Jai <jainaraya@gmail.com>
2022-10-14 16:33:39 -0400

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        mono: ["'DM Mono'", ...defaultTheme.fontFamily.mono],
+        ubuntu: ["'Ubuntu'"],
+        open_sans: ["'Open Sans'", ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         'primary':{
-          'gray': '#262623',
-          'orange': '#FFB400'
+          'gray': '#1D202D',
+          'orange': '#01FFF4'
+        },
+        'secondary':{
+          'white': '#FDFEFF',
         }
       }
     },
