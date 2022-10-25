856ed715d0f188b10192406b4719ed3ba6b14630
JaiNarayanan <47904405+JaiNarayanan@users.noreply.github.com>
2022-10-25 19:00:04 -0400

    color updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,22 +8,22 @@ module.exports = {
     extend: {
       fontFamily: {
         mono: ["'DM Mono'", ...defaultTheme.fontFamily.mono],
-        ubuntu: ["'Ubuntu'"],
         open_sans: ["'Open Sans'", ...defaultTheme.fontFamily.sans],
         proxima_nova: ['Proxima Nova', 'sans-serif'],
-
+        scp: ['Source Code Pro', 'monospace'],
       },
       colors: {
         'primary':{
-          'gray': '#0A253B',
-          'blue': '#1FC1C3'
+          'gray': '#151522',
+          'blue': '#27e3e6',
+          'hover': '#212131'
         },
         'secondary':{
           'border': '#304973',
         },
         'fonts':{
           'white': '#FDFEFF',
-          'gray': '#95A3B1',
+          'gray': '#a5bdc7',
           // 8892B0
         }
       }
